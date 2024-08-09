c186e1498f5e151455a25bb6a8923c5915ec898a
Pryscilla Romão de Medeiros <pryscillaromao@hotmail.com>
2024-08-08 21:02:16 -0300

    Add files via upload

---
+++ exerciciolista1/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace exerciciolista1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int quartos = 0;
+        
+        System.Console.Write("Quantos quartos serão alugados? ");
+        quartos = int.Parse(Console.ReadLine());
+
+        Estudante[] estudantes = new Estudante[10];
+
+        for(int i = 0; i < quartos; i++){
+            string name;
+            string email;
+            int q = 0;
+
+            System.Console.WriteLine("Aluguel #{0}", i+1);
+            System.Console.Write("Nome: ");
+            name = Console.ReadLine();
+
+            System.Console.Write("Email: ");
+            email = Console.ReadLine();
+
+            System.Console.Write("Quarto: ");
+            q = int.Parse(Console.ReadLine());
+
+            if (estudantes[q] == null){
+                estudantes[q] = new Estudante(name, email);
+            }else{
+                System.Console.WriteLine("Quarto preenchido, tente novamente!");
+                i--;
+            }
+            System.Console.WriteLine();
+        }
+        System.Console.WriteLine("Quartos ocupados: ");
+        for(int i = 0; i < estudantes.Length; i++){
+            if(estudantes[i] != null){
+                System.Console.WriteLine("{0}: {1}, {2}", i, estudantes[i].Name, estudantes[i].Email);
+            }
+        }
+    }
+}
