9595d6ddee08fbd5afccca0fff2243f416a2ea42
Pryscilla Romão de Medeiros <pryscillaromao@hotmail.com>
2024-08-22 20:33:19 -0300

    Add files via upload

---
+++ aula72/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace aula72;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int s1 = Calculator.Sum(1, 2);
+        
+        System.Console.WriteLine("{0}", s1);
+    }
+}
